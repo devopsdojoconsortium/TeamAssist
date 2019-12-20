941ab8790952d454ba43c1b2efdba23edf945191
Drew Deal <drewdeal@gmail.com>
2019-12-20 05:30:01 -0600

    made multiple excel tabs. One for each meta type, plus custom if params are sent

--- excelService/excelProc.js
+++ excelService/excelProc.js
@@ -2,14 +2,11 @@ const Excel = require('exceljs');
 import {validRoutes} from '../src/menuRoutes'
 import {tableConfig} from '../src/tableViewConfig'
 import {buildStateObj} from '../src/model'
+import {mutate} from '../src/frpHelpers'
 
 function excelProc(req, events, evtCnt){
-  console.log(validRoutes.teams.meta)
 
-  const routeMeta = validRoutes.teams[req.tab] ? validRoutes.teams[req.tab].meta : validRoutes.teams.meta
-  const tableCols = tableConfig[req.tab] || { cols: []}
   const stateObj = buildStateObj({}, { teams: events}, "teams", { latest: evtCnt, returnState: true})
-  const teamIds = req.teamIds ? req.teamIds.split(/\W+/) : stateObj.ids
   var workbook = new Excel.Workbook({
     modified: new Date(),
     creator: 'Me',
@@ -19,38 +16,60 @@ function excelProc(req, events, evtCnt){
         firstSheet: 0, activeTab: 1, visibility: 'visible'
       } ]
   });
-  var sheet = workbook.addWorksheet(routeMeta.name + ' Sheet', {properties:{tabColor:{argb:'FFC0000'}}});
-  sheet.columns = tableCols.cols.filter(x => x.dKey).map(c => {
-    return {
-      header: c.label,
-      key: c.dKey,
-      width: parseInt((c.width || c.minWidth || 90) / 6)
-    }
-  })
-   // console.log('stateObj and teamIds', stateObj, teamIds)
-  const rows = teamIds.map(tid => {
-    const out = {}
-    const rec = stateObj.state[tid]
-    // console.log('tid', tid, rec)
-    tableCols.cols.forEach(c => {
-      if (c.dKey && rec[c.dKey]) {
-        const val = rec[c.dKey]
-        if (c.hashMap)
-          out[c.dKey] = c.hashMap[val]
-        else if (c.dateFormat)
-          out[c.dKey] = new Date(val * 60000)
-        else if (c.tdStyle === "#modal_priors_cell.mClick.clickBg")
-          out[c.dKey] = accumPriors(rec, events, c.dKey).join("\n ________________________ \n\n")
-        else 
-          out[c.dKey] = val
+  const tabArray = ["teams", "teamLeads", "teamsCompleted"]
+  if(req.tab && req.teamIds)
+    tabArray.push(req.tab)
+  const counts = {}
+  // do sheets in a loopof metas from menuRoutes
+  tabArray.forEach((tab, i) => {
+    const custom = i === 3 ? mutate(req, { name: "Selected Records"}) : {}
+    const routeMeta = validRoutes.teams[tab] ? validRoutes.teams[tab].meta : validRoutes.teams.meta
+    const tableCols = tableConfig[tab] || { cols: []}
+    const teamIds = req.teamIds && custom.tab ? req.teamIds.split(/\W+/) : stateObj.ids
+
+console.log("tab, custom,", tabArray, tab, custom)
+    var sheet = workbook.addWorksheet((custom.name || routeMeta.name), {properties:{tabColor:{argb:'FFC0000'}}});
+    sheet.columns = tableCols.cols.filter(x => x.dKey).map(c => {
+      return {
+        header: c.label,
+        key: c.dKey,
+        width: parseInt((c.width || c.minWidth || 90) / 6)
       }
     })
-    return out
+     // console.log('stateObj and teamIds', stateObj, teamIds)
+    const rows = teamIds
+      .filter(x => custom.name || routeMeta.hardFilt.status.some(s => s === stateObj.state[x].status)) 
+      .map(tid => {
+        const out = {}
+        const rec = stateObj.state[tid]
+        // console.log('tid', tid, rec)
+        tableCols.cols.forEach(c => {
+          if (c.dKey && rec[c.dKey]) {
+            const val = rec[c.dKey]
+            if (c.hashMap)
+              out[c.dKey] = c.hashMap[val]
+            else if (c.dateFormat)
+              out[c.dKey] = new Date(val * 60000)
+            else if (c.tdStyle === "#modal_priors_cell.mClick.clickBg")
+              out[c.dKey] = accumPriors(rec, events, c.dKey).join("\n ________________________ \n\n")
+            else 
+              out[c.dKey] = val
+          }
+        })
+        return out
+      })
+
+    sheet.addRows(rows)
+    counts[tab + i] = rows.length
+
   })
-  sheet.addRows(rows)
-  workbook.xlsx.writeFile("/tmp/test.xlsx").then(excelOut => console.log(excelOut))
-  return JSON.stringify(rows, null, 2)
+
+  // write to file in dist
+  workbook.xlsx.writeFile("./dist/test.xlsx").then(excelOut => console.log("excelOut", excelOut))
+  return JSON.stringify(counts, null, 2)
+
 }
+
 function accumPriors (obj, events, prop) {
   const priorKeys = obj.priors ? [obj.eId].concat(obj.priors.reverse()) : [obj.eId]
   return priorKeys
@@ -58,4 +77,5 @@ function accumPriors (obj, events, prop) {
     .filter(x => x[prop] )
     .map(res =>  res[prop] + "\n\n" + new Date(res.eStamp * 60000).toDateString() + "| " + (res.user || ""))
 }
+
 export {excelProc}
